4726b65d6a9df1cdb7deab66c490aaef78609f1b
GuiDevloper <guicorrea384@gmail.com>
2021-05-19 01:47:36 -0300

    Add cmd runner

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,22 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+
+function showExit(...message) {
+  console.log(...message);
+  process.exit(0);
+}
+
+function runCLI(args, cli, stdio = 'pipe') {
+  const pawn = child.spawnSync(
+    cli,
+    args.split(' '),
+    { encoding: 'utf8', stdio }
+  );
+  if (pawn.stderr) showExit(pawn.stderr);
+  return pawn;
+}
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  runCLI('build', 'npm', 'inherit');
 } catch (error) {
   core.setFailed(error.message);
 }
